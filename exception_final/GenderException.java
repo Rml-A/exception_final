a6ccb988bfe51aadcdbedc2820c8b3f0a1d15dd5
Ramil Akhmetov <ramiltat777@gmail.com>
2023-10-10 21:15:22 +0300

    final exception

---
+++ exception_final/GenderException.java
@@ -0,0 +1,9 @@
+package exception_final;
+
+public class GenderException extends Exception {
+    public GenderException() {
+        super("Введены некорректные данные для значения 'пол'");
+    }
+}
+    
+
